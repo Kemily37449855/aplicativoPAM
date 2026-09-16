e17eef91ac458878d4d026637a77b85b5be8794b
pcetec <pc@etec.com>
2026-09-16 20:46:55 -0300

    Manipulando elementos pelo kotlin

--- app/src/main/java/com/example/aplicativopan/MainActivity.kt
+++ app/src/main/java/com/example/aplicativopan/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.aplicativopan
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.exercicio_04_gravity)
+        setContentView(R.layout.aulaid)
+
+        val botao = findViewById<Button>(R.id.btnProxima)
+        val texto = findViewById<EditText>(R.id.editValor)
+
+        botao.text = "Novo Texto"
+
+        botao.setOnClickListener {
+
+           botao.text = texto.text
+        }
 
     }
 }
